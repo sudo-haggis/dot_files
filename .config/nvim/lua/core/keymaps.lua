67ac41e1bbdf95613c887dfc2a8d4cd9aa50c3cd
weedavedev <156850571+sudo-haggis@users.noreply.github.com>
2025-05-12 13:55:05 +0200

    Phase 1: Added numbering, basic keymaps, mouse, tab indentation etc. Basic bitch nvim options

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,5 @@
+-- Basic window navigation (without TMUX integration)
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { silent = true, noremap = true })
