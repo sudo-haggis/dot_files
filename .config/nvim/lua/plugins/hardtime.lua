37a1cc5db440a710b5fefd9e7f4e6f59796938de
sudo-haggis <156850571+sudo-haggis@users.noreply.github.com>
2025-05-25 21:39:29 +0200

    feat(hardtimevim): add hard time vim plugin to config

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+--hard time configuration
+
+local M = {}
+
+function M.setup()
+    vim.g.hardtime_default_on = 1   --enabled by deault 
+    vim.g.hardtime_showmsg = 1      --show the warning message
+    vim.g.hardtime_allow_different_key = 1  --allow different keys
+    vim.g.hardtime_maxcount = 3     -- Max repetitions before blockin'
+end
+
+return M
+    
