b5007c0bae16489db13ee969c4197ba99f50fc49
sudo-haggis <156850571+sudo-haggis@users.noreply.github.com>
2025-09-27 08:25:33 +0200

    chore(hardtime): adjust count of presseseses

--- nvim/.config/nvim/lua/plugins/hardtime.lua
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -6,7 +6,7 @@ function M.setup()
 	vim.g.hardtime_default_on = 1 --enabled by deault
 	vim.g.hardtime_showmsg = 1 --show the warning message
 	vim.g.hardtime_allow_different_key = 1 --allow different keys
-	vim.g.hardtime_maxcount = 2 -- Max repetitions before blockin'
+	vim.g.hardtime_maxcount = 5 -- Max repetitions before blockin'
 end
 
 M.setup()
