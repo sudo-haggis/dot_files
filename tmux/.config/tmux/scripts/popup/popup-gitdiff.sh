80baa1c46795196a7f84e3a806c53ee343d8f864
sudo-haggis <156850571+sudo-haggis@users.noreply.github.com>
2026-03-15 16:54:43 +0000

    feat(tpop_gitdiff): implement the git diff functionality script <file> <file> or script <file> <branch> <branch>

--- tmux/.config/tmux/scripts/popup/popup-gitdiff.sh
+++ tmux/.config/tmux/scripts/popup/popup-gitdiff.sh
@@ -6,14 +6,26 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# if no params then remind of the ways of the doing!
 if [[ -z "$1" ]]; then
     echo "Usage: popup-gitdiff.sh <filename> <branch1> <branch2> in TMUX"
     exit 1
 fi
 
-FILE="$1"
-BRANCH1="$2"
-BRANCH2="$3"
+#If the second param is a file, and there is only 2 params;
+# it must be a comparrison of two ranom files situation
+if [[ -f "$2" ]] && (( $# == 2 )); then
+    FILE1="$1"
+    FILE2="$2"
 
-"$SCRIPT_DIR/../tmux-popup.sh" -d "$(pwd)" "nvim -d <(git show $BRANCH1:$FILE) <(git show $BRANCH2:$FILE)"
+    "$SCRIPT_DIR/../tmux-popup.sh" -d "$(pwd)" "nvim -d $FILE1 $FILE2"
+fi
 
+#must be a single file and two branches sorta thing !
+if [[ -f "$1" ]] && [[ ! -z "$2" ]] && [[ ! -z "$3" ]]; then
+    FILE="$1"
+    BRANCH1="$2"
+    BRANCH2="$3"
+
+    "$SCRIPT_DIR/../tmux-popup.sh" -d "$(pwd)" "nvim -d <(git show $BRANCH1:$FILE) <(git show $BRANCH2:$FILE)"
+fi
